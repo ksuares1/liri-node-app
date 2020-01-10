c9109f353df74d0b69ea12e2733c04a295303fdc
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-01-10 15:58:38 -0500

    added console to liri

--- assets/JavaScript/liri.js
+++ assets/JavaScript/liri.js
@@ -1,3 +1,11 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
+
+// This will print everything in exports.
+console.log("--------------------------");
+console.log("Title of the movie is" + music.bands.punk);
+console.log("Year the movie came out"+ music.bands.rap);
+console.log("IMDB Rating of the Movie" +music.bands.classic);
+console.log("--------------------------");
+
