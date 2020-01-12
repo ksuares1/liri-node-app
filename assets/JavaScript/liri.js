b22bc0cd43607e1279c42b6c0a0855997136830b
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-01-12 15:56:38 -0500

    added some code liri file

--- assets/JavaScript/liri.js
+++ assets/JavaScript/liri.js
@@ -2,10 +2,4 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 
-// This will print everything in exports.
-console.log("--------------------------");
-console.log("Title of the movie is" + music.bands.punk);
-console.log("Year the movie came out"+ music.bands.rap);
-console.log("IMDB Rating of the Movie" +music.bands.classic);
-console.log("--------------------------");
-
+var userInput = process.argv[2];
