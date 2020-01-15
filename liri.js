be36f66b1e8e8917e43ad87ba7d4fae1022b9416
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-01-14 22:14:26 -0500

    removed and added new files

---
+++ liri.js
@@ -0,0 +1,28 @@
+var axios= require('axios');
+require("dotenv").config();
+
+var Spotify = require('node-spotify-api');
+var keys = require("./assets/JavaScript/keys.js");
+var spotify = new Spotify(keys.spotify);
+
+// commands
+var command = process.argv[2];
+var input = process.argv.slice(3).join(' ');
+if (command === "concert-this") {
+
+    axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp")
+        .then(function (response) {
+            for(var i=0; i<response.data.length; i++){
+                console.log(response.data[i].venue); 
+            }
+           
+        })
+
+        .catch(function (error) {
+            console.log(error);
+        })
+
+}else if(command === "spotify-this-song"){
+
+}
+
