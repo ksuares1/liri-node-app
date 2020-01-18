6eb460064c25528a9c6b102487ba81d413513dc3
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-01-18 11:01:50 -0500

    added error to console

--- liri.js
+++ liri.js
@@ -29,19 +29,20 @@ if (command === "concert-this") {
 
 } else if (command === "spotify-this-song") {
     var songName = process.argv[3];
-    axios.get('https://api.spotify.com/v1/search?q=track:' + songName + '&type=track&limit=10')
-        .then(function (error, response) {
-            for (var i = 0; i < response.data.length; i++) {
+    spotify.request('https://api.spotify.com/v1/search?q=track:' + songName + '&type=track&limit=10', function (error, response) {
+             for (var i = 0; i < response.data ; i++) {
                 console.log(response.data[i].artists.name);
-                console.log(response.data[i].artists.name.song);
-                console.log(response.data[i].artists.name.preview_url);
-                console.log(response.data[i].artists.name);
+                // console.log(response.data[i].artists.name.song);
+                // console.log(response.data[i].artists.name.preview_url);
+                // console.log(response.data[i].artists.name);
                 
             }
-        }
-
+        
+            if (error){
+                return console.log(error);
+            }
     
     
 
-        )
+    })
     }
